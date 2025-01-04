89879ac0a194f5f26e0078f242014a3573e18f47
Bohan Xu <bohanx@andrew.cmu.edu>
2025-01-04 01:38:54 -0800

    fix bug

--- getData.sh
+++ getData.sh
@@ -5,8 +5,9 @@ python3 -m venv diffusionEnv
 source diffusionEnv/bin/activate
 pip install -r requirements.txt
 gdown https://drive.google.com/uc?id=1ZIfi3Tqv_KQBBJruoQ_VA90mesWoZQrI
-gdown https://drive.google.com/uc?id=1qc2Gq6vdTTXhtu1qG629jbKbPaduHf6d
+gdown https://drive.google.com/uc?id=1gjWrgfcw7UBquKuR7icQpCmZHZ6BEquH
+
 sudo apt update
 sudo apt install unrar-free
-unrar x imagenet100_128x128.rar DiffusionHW5/data -y
+unzip imagenet100_128x128.zip -d data
 nvidia-smi
