02126db6bb6edd40239ac46d7569af05a5a5e746
Bohan Xu <bohanx@andrew.cmu.edu>
2025-01-04 00:52:01 -0800

    change config

--- getData.sh
+++ getData.sh
@@ -8,5 +8,5 @@ gdown https://drive.google.com/uc?export=download&id=1ZIfi3Tqv_KQBBJruoQ_VA90mes
 gdown https://drive.google.com/uc?id=1qc2Gq6vdTTXhtu1qG629jbKbPaduHf6d
 sudo apt update
 sudo apt install p7zip-full
-7z x imagenet100_128x128.rar  -o/data
+7z x imagenet100_128x128.rar  -odata
 nvidia-smi
