101b2b5730f193fc902964a3f0a49788cc98c703
Bohan Xu <bohanx@andrew.cmu.edu>
2025-01-04 01:20:01 -0800

    fix bug

--- getData.sh
+++ getData.sh
@@ -4,7 +4,7 @@ mkdir pretrained
 python3 -m venv diffusionEnv
 source diffusionEnv/bin/activate
 pip install -r requirements.txt
-gdown https://drive.google.com/uc?export=download&id=1ZIfi3Tqv_KQBBJruoQ_VA90mesWoZQrI
+gdown https://drive.google.com/uc?id=1ZIfi3Tqv_KQBBJruoQ_VA90mesWoZQrI
 gdown https://drive.google.com/uc?id=1qc2Gq6vdTTXhtu1qG629jbKbPaduHf6d
 sudo apt update
 sudo apt install p7zip-full
