497081f0a16f62ba2b69b5d8f15f2b0b3071ad1b
Bohan Xu <bohanx@andrew.cmu.edu>
2025-01-04 01:23:45 -0800

    fix bug

--- getData.sh
+++ getData.sh
@@ -7,6 +7,6 @@ pip install -r requirements.txt
 gdown https://drive.google.com/uc?id=1ZIfi3Tqv_KQBBJruoQ_VA90mesWoZQrI
 gdown https://drive.google.com/uc?id=1qc2Gq6vdTTXhtu1qG629jbKbPaduHf6d
 sudo apt update
-sudo apt install p7zip-full
-7z x imagenet100_128x128.rar  -odata
+sudo apt install unrar-free
+unrar x imagenet100_128x128.rar DiffusionHW5/data -y
 nvidia-smi
